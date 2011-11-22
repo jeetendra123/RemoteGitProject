8a1b68376faa2aa85fc76307613a6128e2d8f62c
jeetendra123 <jeetendra.prusty@cmcltd.com>
2011-11-22 12:22:54 +0530

    Update RemoteGitProject/src/HelloRemote.java

--- RemoteGitProject/src/HelloRemote.java
+++ RemoteGitProject/src/HelloRemote.java
@@ -7,6 +7,8 @@ public class HelloRemote {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World !");
 		System.out.println("Hello World Remote Edit!");
+		
+		System.out.println("Hello World Second Remote Edit!");
 	}
 
 }
